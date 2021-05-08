3cb8debafadb19bcddc31f9ccb2dfb01e11bb662
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-05-07 22:06:44 -0400

    Added On_Guild_Join Support

---
+++ cogs/automated/guild.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+from dhooks import Webhook
+
+class Guild(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+      hook = Webhook('https://discord.com/api/webhooks/840407485973397504/PjCzZSld9GwAiCfL7QcyXqhb_8PurN3lXqM-w0fpns6ZkcgQuqCaLCQTjnmrpNPEXgDP')
+      embed = discord.Embed(title=f'DisSlash Has Been Added To A New Guild!', description=f'We Are Now At {len(client.guilds)}!')
+      hook.send(embed=embed)
+      
+      
+def setup(client):
+    client.add_cog(Guild(client))

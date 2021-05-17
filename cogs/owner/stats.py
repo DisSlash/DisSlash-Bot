3e4bae0dc1dbcb80c29853f8bad7b4f2801a1e8d
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-05-17 08:55:07 -0400

    Added Time Support

--- cogs/owner/stats.py
+++ cogs/owner/stats.py
@@ -5,26 +5,26 @@ from datetime import datetime
 
 
 now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
+current = now.strftime("%H:%M:%S")
 
 class Stats(commands.Cog):
   
   def __init__(self, client):
         self.client = client
-      
-  @commands.Cog.listner()
-  async def on_ready(self):
-    print("I Have Been Summoned")
-    global startTime
-    startTime = time.time()
   
   
   @commands.command()
   async def stats(self, ctx):
-    uptime = str(datetime.timedelta(seconds=int(round(time.time()-startTime))))
+    
+    # Getting Time
+    current_time = time.time()
+    difference = int(round(current_time - start_time))
+    uptime = str(datetime.timedelta(seconds=difference))
+    
+    # Embed
     embed = discord.Embed(title=f'Hey! This Is My Stats Since Last Check')
     embed.set_thumbnail(url='https://i.imgur.com/US4aSgW.png')
-    embed.set_footer(text=f'The Time This Command Was Requested Was {current_time}')
+    embed.set_footer(text=f'The Time This Command Was Requested Was {current}')
     embed.add_feild(name="Uptime", value=f'DisSlash Has Been Online For {uptime}')
     embed.add_feild(name="Servers", value=f'DisSlash Is In {len(client.guilds)} Servers')
     await ctx.send(embed=embed)

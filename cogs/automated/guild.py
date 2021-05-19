df6f4fc74697ef14098e5f784d40ff759d59ec51
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-05-19 14:13:37 -0400

    Updated Format

--- cogs/automated/guild.py
+++ cogs/automated/guild.py
@@ -10,16 +10,16 @@ class Guild(commands.Cog):
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
       hook = Webhook('https://discord.com/api/webhooks/840407485973397504/PjCzZSld9GwAiCfL7QcyXqhb_8PurN3lXqM-w0fpns6ZkcgQuqCaLCQTjnmrpNPEXgDP')
-      name= guild.name
-      owner = guild.owner
-      guildid = guild.id
-      memberCount = guild.member_count
-      icon = guild.icon_url
+      name= str(guild.name)
+      owner = str(guild.owner)
+      guildid = str(guild.id)
+      memberCount = str(guild.member_count)
+      icon = str(guild.icon_url)
       
       embed = discord.Embed(title="DisSlash Is In A New Guild!")
-      embed.add_feild(name="Guild Name", value=f'{name}')
-      embed.add_feild(name="Guild ID", value=f'{guildid}')
-      embed.add_feild(name="Member Count", value=f'{memberCount}')  
+      embed.add_field(name="Guild Name", value=f'{name}')
+      embed.add_field(name="Guild ID", value=f'{guildid}')
+      embed.add_field(name="Member Count", value=f'{memberCount}')  
       embed.set_thumbnail(url=icon)
       hook.send(embed=embed)
       

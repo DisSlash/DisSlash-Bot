1ddbe01fb17ee1f7eb8b921fe0fb39bd25e1d037
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-05-19 14:08:18 -0400

    Updated Formatting

--- cogs/automated/guild.py
+++ cogs/automated/guild.py
@@ -10,7 +10,17 @@ class Guild(commands.Cog):
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
       hook = Webhook('https://discord.com/api/webhooks/840407485973397504/PjCzZSld9GwAiCfL7QcyXqhb_8PurN3lXqM-w0fpns6ZkcgQuqCaLCQTjnmrpNPEXgDP')
-      embed = discord.Embed(title=f'DisSlash Has Been Added To A New Guild!', description=f'Congrats DisSlash Team!')
+      name= guild.name
+      owner = guild.owner
+      guildid = guild.id
+      memberCount = guild.member_count
+      icon = guild.icon_url
+      
+      embed = discord.Embed(title="DisSlash Is In A New Guild!")
+      embed.add_feild(name="Guild Name", value=f'{name}')
+      embed.add_feild(name="Guild ID", value=f'{guildid}')
+      embed.add_feild(name="Member Count", value=f'{memberCount}')  
+      embed.set_thumbnail(url=icon)
       hook.send(embed=embed)
       
       

e4c7a36697325b2601a153ddec35323994973eca
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-05-19 14:38:32 -0400

    Updated Webhook

--- cogs/automated/guild.py
+++ cogs/automated/guild.py
@@ -9,7 +9,7 @@ class Guild(commands.Cog):
     
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-      hook = Webhook('https://discord.com/api/webhooks/840407485973397504/PjCzZSld9GwAiCfL7QcyXqhb_8PurN3lXqM-w0fpns6ZkcgQuqCaLCQTjnmrpNPEXgDP')
+      hook = Webhook('https://discord.com/api/webhooks/844645105235132436/KJwdVbkrYLeD8EKPhQsvKK2uRUR7nSH9lysHJC2hsH1Esr9-97zEl2UPcbhBmiCKwSrr')
       name= str(guild.name)
       owner = str(guild.owner)
       guildid = str(guild.id)

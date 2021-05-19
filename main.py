7d95afd1aabe29e7189acb2d1c44b5d9d1f7c21a
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-05-18 21:26:29 -0400

    Added topgg.DBL

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ client.remove_command('help')
 # Load TOPGG
 
 dbl_token  = os.environ["TOPTOKEN"]
-client.topggpy = dbl.DBLClient(client, dbl_token)
+client.topggpy = topgg.DBLClient(client, dbl_token)
 
 # Loop
 async def status_task():

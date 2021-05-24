86b71656bac48a400292ce90bcaca4b330cc1a21
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-05-24 19:47:52 -0400

    Updated Code

--- cogs/text/notepad.py
+++ cogs/text/notepad.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
 from discord_slash.utils.manage_commands import create_option, create_choice
 from pymongo.common import validate_uuid_representation
+from pymongo.uri_parser import _handle_option_deprecations
 
 MONGODB = os.environ["MONGODB"]
 cluster = MongoClient(MONGODB)
@@ -36,19 +37,19 @@ class NotePad(commands.Cog):
               )
               ]),
             create_option(
-              name="content",
-              description="Please Enter The Body Of The Note",
-              option_type=3,
-              required=True
-            ),
-            create_option(
               name="name",
               description="Enter The Name Of The Note",
               option_type=3,
               required=True
+            ),
+            create_option(
+              name="content",
+              description="Please Enter The Body Of The Note",
+              option_type=3,
+              required=False
             )
           ])
-    async def notepad(self, ctx, action: str, content: str, name: str):
+    async def notepad(self, ctx, action: str, name: str, *, content=None):
       author = ctx.author.id
       docCount = notes.count_documents({})
       noteName = name.lower()
@@ -60,13 +61,11 @@ class NotePad(commands.Cog):
 
         # Embed
         embed = discord.Embed(title=f'A New Note Has Been Made!')
-        embed.add_field(name="Name Of Note", value=f'{name}')
-        embed.add_field(name="Content", value=f'{content}')
+        embed.add_field(name="Name Of Note", value=f'{name}', inline=False)
+        embed.add_field(name="Content", value=f'{content}', inline=False)
         await ctx.send(embed=embed)
 
       elif action == "view":
-        
-        try:
           search = notes.find({"user": author})
 
           for query in search:
@@ -74,15 +73,19 @@ class NotePad(commands.Cog):
 
           if noteName in nameNote:
             noteFinal = notes.find({"name": noteName})
+
             for query in noteFinal:
-              nameOfNote = query['name']
               contentOfNote = query['note']
+
             embed = discord.Embed(title=f'New Note Query')
-            embed.add_field(name=f'Note Name: {nameOfNote}', vlaue=f'{contentOfNote}')
+            embed.add_field(name=f'Note Name', value=f'{noteName}', inline=False)
+            embed.add_field(name="Note Content", value=f"{contentOfNote}", inline=False)
             await ctx.send(embed=embed)
 
           else:
-            await ctx.send("That Is Not A Valid Note!", hidden=True)
-        except:
-          await ctx.send("There Has Been An Error, Please DM Neil Shah#6469", hidden=True)
+            await ctx.send("This Is Not A Valid Note", hidden=True)
+
+
+def setup(client):
+    client.add_cog(NotePad(client))
         

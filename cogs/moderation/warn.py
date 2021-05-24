d85b7023c891a7408eb3af90e1dab76031c25eb2
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-05-24 14:02:40 -0400

    WIP

--- cogs/moderation/warn.py
+++ cogs/moderation/warn.py
@@ -1,8 +1,19 @@
 import discord
+import os
+import pymongo
+from pymongo import MongoClient
 from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
 from discord_slash.utils.manage_commands import create_option, create_choice
 
+# Database Setup
+MONGODB = os.environ["MONGODB"]
+cluster = MongoClient(MONGODB)
+
+db = cluster['disslash']
+warns = db['warns']
+
+# Start Of Command
 class Warn(commands.Cog):
 
     def __init__(self, client):
@@ -11,6 +22,21 @@ class Warn(commands.Cog):
     @cog_ext.cog_slash(name="warn",
              description="Warn A Member From This Server",
              options=[
+               create_option(
+                 name="action",
+                 description="Pick To Either Warn A User, Or View Warns (If Warning, You Have To Add Reason)",
+                 option_type=3,
+                 required=True
+                 choices=[
+                  create_choice(
+                    name="warn",
+                    value="warn"
+                  ),
+                  create_choice(
+                    name="logs",
+                    value="logs"
+                  )
+               ),
                create_option(
                  name="member",
                  description="This Command Will Only Work If You Are A Mod",
@@ -21,15 +47,29 @@ class Warn(commands.Cog):
                  name="reason",
                  description="Add A Reason For Warning This User",
                  option_type=3,
-                 required=True
+                 required=False
                )
              ])
     @commands.has_permissions(kick_members=True)
     @commands.guild_only()
-    async def warn(self, ctx, member: discord.Member, *, reason=None):
-        embedVar = discord.Embed(description=f'{member.mention} has been warned')
-        await ctx.send(embed=embedVar)
-        await member.send(f'You have been warned, Reason: {reason}')
+    async def warn(self, ctx, action: str, member: discord.Member, reason=None):
+        if action == "warn":
+                 memberid = member.id
+                 post = {"_id": count + 1, "user": memberid, "reason": reason}
+                 warns.insert_one(post)
+                 
+                 embedVar = discord.Embed(description=f'{member.mention} has been warned')
+                 await ctx.send(embed=embedVar)
+                 
+                 await member.send(f'You have been warned, Reason: {reason}')
+                 
+        elif action == "logs":
+                 search = warns.find({"user": memberid})
+                 for query in search:
+                    call = warns['_id']
+                    user = warns['user']
+                    reason = warns['reason']
 
+                 numberReason = len(reason)
 def setup(client):
     client.add_cog(Warn(client))

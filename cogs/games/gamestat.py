c8f2e6411c9f228fb376343bedcb670d9322d811
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-05-25 15:33:46 -0400

    Update gamestat.py

--- cogs/games/gamestat.py
+++ cogs/games/gamestat.py
@@ -72,7 +72,7 @@ class GameStat(commands.Cog):
 
         # Duo
         duoWins = duo['wins']
-        duoTop3 = duo['top10']
+        duoTop3 = duo['top5']
         duoKills = duo['kills']
      
         # Title
@@ -90,7 +90,7 @@ class GameStat(commands.Cog):
         
         # Duo
         embed.add_field(name="Duo Wins", vlaue=f'{name} Has Won {duoWins} Games')
-        embed.add_field(name="Duo Top 10", value=f'{name} Has Been On The Leaderboard {duoTop3} Games')
+        embed.add_field(name="Duo Top 5", value=f'{name} Has Been On The Leaderboard {duoTop3} Games')
         embed.add_field(name="Duo Kills", vlaue=f'{name} Has Killed {duoKills} Players')
         
         await ctx.send(embed=embed)

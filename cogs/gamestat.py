f7b077d15a97d3cb28c95075893196279d3f255b
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-05-25 14:00:48 -0400

    Create gamestat.py

---
+++ cogs/gamestat.py
@@ -0,0 +1,87 @@
+import discord
+import fortnite_api
+from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
+from discord_slash.utils.manage_commands import create_option, create_choice
+
+# Starting API
+fortnite = fortnite_api.FortniteAPI()
+
+class GameStat(commands.Cog):
+  
+  def __init__(self, client):
+    self.client = client
+  
+  @cog_ext.cog_slash(name="gamestat",
+          description="Get Your Game Stats Right From Discord",
+          options=[
+            create_option(
+              name="game",
+              description="Pick The Game You Want To Get Stats From (More Comming)",
+              option_type=3,
+              required=True,
+              choices=[
+              create_choice(
+                name="fortnite",
+                value="fortnite"
+              ),
+              create_choice(
+                name="hypixel",
+                value="hypixel"
+              )
+              ]),
+            create_option(
+              name="username",
+              description="Enter The Username Of The Player (Not Case Sensitive)",
+              option_type=3,
+              required=True
+            )
+          ])
+  async def gamestat(self, ctx, game: str, username=None):
+    username = username.lower()
+    if game == "fortnite":
+      try:
+        # Get User Data And Dict
+        stats = api.stats.fetch_by_name(username).stats
+        battle_pass = api.stats.fetch_by_name(username).battle_pass
+        name = api.stats.fetch_by_name(username).user
+        data = stats.raw_data
+        battle = battle_pass.raw_data
+
+        # Narrow Down Serach Results
+        data = data['all']
+        overall = data['overall']
+        solo = data['solo']
+        duo = data['duo']
+
+        # Colleting Used Data
+    
+        # Battle Pass/Account
+        level = battle['level']
+        name = name.name
+        
+        # Overalll
+        overallWins = overall['wins']
+        overallTop3 = overall['top3']
+        overallKills = overall['kills']
+
+        # Solo
+        soloWins = solo['wins']
+        soloTop3 = solo['top3']
+        soloKills = solo['kills']
+
+        # Duo
+        duoWins = duo['wins']
+        duoTop3 = duo['top3']
+        duoKills = duo['kills']
+      
+        # Embed
+        embed = discord.Embed(title=f'Player Stats For {name}')
+        # wip
+    except:
+      pass
+
+except fortnite_api.NotFound:
+    print("Not A Player")
+    elif game == "hypixel":
+      pass

efaae250442f114d70a1957d3a1bb56c94eab73e
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-06-04 16:46:09 -0400

    Update help(regular).py

--- cogs/help/help(regular).py
+++ cogs/help/help(regular).py
@@ -1,6 +1,7 @@
 import discord
 import os
 from discord.ext import commands
+from discord_components import DiscordComponents, Button
 
 update = "Hello Everyone, Today I Would Like To Announce That DisSlash Has Officially Been Verified! Now, You Can Add DisSlash To As Many Servers As You Want. If You Need Help, Feel Free To Join Our Support Server, Or Email Us At `info@disslash.me`!"
 
@@ -19,7 +20,11 @@ class HelpRegular(commands.Cog):
         embedVar.add_field(name="`Command Request`", value="To request a command to be added to be added, send a request form [here](https://forms.gle/Y1y8XYTEtsQoPaGq6).", inline=False)
         embedVar.add_field(name="`Support`", value="Need help using the Discord Bot? Join our server [here](https://discord.gg/kPhuc65q2u), and open a ticket.", inline=False)
         embedVar.add_field(name="`Contact Us`", value="Want to contact us to get info about our bot, email us as info@disslash.me.", inline=False)
-        await ctx.send(embed=embedVar)
+        await ctx.send(embed=embedVar, components=[
+            Button(style=ButtonStyle.URL, label="Website", url="https://disslash.me")
+            Button(style=ButtonStyle.URL, label="Invite", url="https://disslash.me/invite"),
+        ])
 
 def setup(client):
+    DiscordComponents(client)
     client.add_cog(HelpRegular(client))

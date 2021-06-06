5f906f673a3b20614e8b06dd5f46c2294b7e5c1a
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-06-06 08:15:36 -0400

    Update help(regular).py

--- cogs/help/help(regular).py
+++ cogs/help/help(regular).py
@@ -6,17 +6,8 @@ from pymongo import MongoClient
 update = "Hello Everyone, Today I Would Like To Announce That DisSlash Has Officially Been Verified! Now, You Can Add DisSlash To As Many Servers As You Want. If You Need Help, Feel Free To Join Our Support Server, Or Email Us At `info@disslash.me`!"
 
 MONGODB = os.environ["MONGODB"]
-cluster = MongoClient(MONGODB)
-db = cluster['disslash']
-news = db['news']
 
 
-count = news.count_documents({})
-
-newsList = news.find({"_id": count})
-
-for news in newsList:
-    news = news['news']
 
 class HelpRegular(commands.Cog):
     def __init__(self, client):
@@ -26,15 +17,20 @@ class HelpRegular(commands.Cog):
     # Help Command
     @commands.command()
     async def help(self, ctx):
+        
+        cluster = MongoClient(MONGODB)
+        db = cluster['disslash']
+        news = db['news']
+
         count = news.count_documents({})
 
         newsList = news.find({"_id": count})
 
-        for news in newsList:
-            news = news['news']
+        for new in newsList:
+            newsUpdate = new['news']
 
         embedVar = discord.Embed(title='Hey! Im DisSlash, I am a Discord Bot that adds Slash Commands to your Discord Server', color=0xFF0000)
-        embedVar.add_field(name="DisSlash News", value=news, inline=False)
+        embedVar.add_field(name="DisSlash News", value=newsUpdate, inline=False)
         embedVar.add_field(name="`Slash Commands`", value="To use slash commands, type `/` into the message box to bring up my commands.", inline=False)
         embedVar.add_field(name="`Invite Bot`", value="Are you loving this Discord Bot? why not invite it to your server [here](https://bit.ly/3ml5Lbf).", inline=False)
         embedVar.add_field(name="`Command Request`", value="To request a command to be added to be added, send a request form [here](https://forms.gle/Y1y8XYTEtsQoPaGq6).", inline=False)

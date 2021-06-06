47e976dca117ad1c3b7b4daf758b99f540a5f56e
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-06-06 08:05:27 -0400

    Update news.py

--- cogs/owner/news.py
+++ cogs/owner/news.py
@@ -12,21 +12,20 @@ class News(commands.Cog):
   
   @commands.command()
   async def news(self, ctx, news):
+    
     author = ctx.author.id
     cluster = MongoClient(MONGODB)
     db = cluster['disslash']
     news = db['news']
     count = news.count_documents({})
-    try:
-      post = {"_id": count + 1, "news": news}
-      notes.insert_one(post)
-
-      embed = discord.Embed(title="New News Update")
-      embed.add_field(name="`New News`", value = news)
-      await ctx.send(embed=embed)
     
-    except:
-      await ctx.send("Post Failed To Send, Please Fix")
+    post = {"_id": count + 1, "news": news}
+    notes.insert_one(post)
+
+    embed = discord.Embed(title="New News Update")
+    embed.add_field(name="`New News`", value = news)
+    await ctx.send(embed=embed)
+
       
       
 def setup(client):

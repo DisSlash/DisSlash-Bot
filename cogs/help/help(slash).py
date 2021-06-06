5d94f55ea2727c009231e4ed9447691279df6243
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-06-06 08:21:41 -0400

    Update help(slash).py

--- cogs/help/help(slash).py
+++ cogs/help/help(slash).py
@@ -2,9 +2,11 @@ import discord
 import os
 from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
+from pymongo import MongoClient
 
 update = "Hello Everyone, Today I Would Like To Announce That DisSlash Has Officially Been Verified! Now, You Can Add DisSlash To As Many Servers As You Want. If You Need Help, Feel Free To Join Our Support Server, Or Email Us At `info@disslash.me`!"
 
+MONGODB = os.environ["MONGODB"]
 
 class HelpSlash(commands.Cog):
     def __init__(self, client):
@@ -15,8 +17,20 @@ class HelpSlash(commands.Cog):
     @cog_ext.cog_slash(name="help",
                 description="Open Our Help Command")
     async def help(self, ctx: SlashContext):
+        
+        cluster = MongoClient(MONGODB)
+        db = cluster['disslash']
+        news = db['news']
+
+        count = news.count_documents({})
+
+        newsList = news.find({"_id": count})
+
+        for new in newsList:
+            newsUpdate = new['news']
+        
         embedVar = discord.Embed(title='Hey! Im DisSlash, I am a Discord Bot that adds Slash Commands to your Discord Server', color=0xFF0000)
-        embedVar.add_field(name="DisSlash News", value=update, inline=False)
+        embedVar.add_field(name="DisSlash News", value=newsUpdate, inline=False)
         embedVar.add_field(name="`Slash Commands`", value="To use slash commands, type `/` into the message box to bring up my commands.", inline=False)
         embedVar.add_field(name="`Invite Bot`", value="Are you loving this Discord Bot? why not invite it to your server [here](https://bit.ly/3ml5Lbf).", inline=False)
         embedVar.add_field(name="`Command Request`", value="To request a command to be added to be added, send a request form [here](https://forms.gle/Y1y8XYTEtsQoPaGq6).", inline=False)

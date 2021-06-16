0c8a7105be71225b88d33e4519c0935395a750e8
Neil Shah <neildshah1013@gmail.com>
2021-06-16 19:32:55 -0400

    Added Reaction Role {(Beta})

---
+++ cogs/automated/reactions.py
@@ -0,0 +1,37 @@
+import os
+import discord
+from pymongo import MongoClient
+from discord.ext import commands
+
+MONGODB = os.environ["MONGODB"]
+cluster = MongoClient(MONGODB)
+db = cluster["disslash"]
+roles = db["roles"]
+
+class Reactions(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+
+        if payload.member.bot:
+            pass
+
+        else:
+            try:
+                data = roles.find({"emoji": payload.emoji.name, "message_id": payload.message_id})
+                for i in data:
+                    role_id = i["role_id"]
+                role = discord.utils.get(self.client.get_guild(payload.guild_id).roles, id=role_id)
+
+                await payload.member.add_roles(role)
+
+            except UnboundLocalError:
+                pass
+    
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        pass
+    
---
+++ cogs/moderation/reactrole.py
@@ -0,0 +1,62 @@
+import os
+import discord
+from discord.ext import commands
+from discord_slash.utils import manage_commands
+from discord_slash.utils.manage_commands import create_choice
+from pymongo import MongoClient
+
+MONGODB = os.environ["MONGODB"]
+cluster = MongoClient(MONGODB)
+db = cluster["disslash"]
+roles = db["roles"]
+
+class ReactRole(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+    
+    @cog_ext.cog_slash(
+        name="reactrole",
+        description="Make A Reaction Role",
+        options=[
+            create_option(
+                name="emoji",
+                description="Pick An Emoji For The Reaction Role",
+                option_type=3,
+                required=True,
+            ),
+            create_option(
+                name="role",
+                description="Pick The Role You Want To Give (DisSlash Must Be Higher)",
+                option_type=8,
+                required=True,
+            ),
+            create_option(
+                name="message",
+                description="Enter The Message You Want The Embed To Contain",
+                option_type=3,
+                required=True,
+            )
+        ],
+    )
+    @commands.has_permissions(manage_server=True)
+    @commands.guild_only()
+    async def reactrole(self, ctx, emoji, role: discord.Role, message):
+        try:
+            embed = discord.Embed(description=message)
+            msg = await ctx.channel.send(embed=embed)
+            await msg.add_reaction(emoji)
+
+            count = roles.count_documents({})
+            new_react_role = {
+                "_id": count + 1,
+                "role_name": role.name,
+                "role_id": role.id,
+                "emoji": emoji,
+                "message_id": msg.id,
+            }
+            
+            roles.insert_one(new_react_role)
+
+        except:
+            await ctx.send("Please Enter Only The Emoji, No Text. If It Still Does Not Work, Please DM Neil Shah!", hidden=True)

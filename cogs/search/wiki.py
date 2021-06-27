37df4721f43290e4ff375a11f7d6e822727f2bf2
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2021-06-27 16:34:30 -0400

    Update wiki.py

--- cogs/search/wiki.py
+++ cogs/search/wiki.py
@@ -21,28 +21,27 @@ class Wikipedia(commands.Cog):
             )
         ],
     )
-    async def wiki(self, ctx, *query):
+    async def wiki(self, ctx, query):
         await ctx.send("I have sent the result to your DM.", hidden=True)
         author = ctx.author
-        if query:
-            query = " ".join(query)
-            wiki_wiki = wikipediaapi.Wikipedia("en")
-            page_py = wiki_wiki.page(query)
-            question = query
-            query = query.replace(" ", "")
+        
+        query = " ".join(query)
+        wiki_wiki = wikipediaapi.Wikipedia("en")
+        page_py = wiki_wiki.page(query)
+        question = query
+        query = query.replace(" ", "")
 
-            if page_py.exists():
-                embedVar = discord.Embed(
-                    title=f"Result:",
-                    description=f"%s" % page_py.summary[0:400]
-                    + f"[...](https://en.wikipedia.org/wiki/{query})",
-                )
-                await author.send(embed=embedVar)
-            else:
-                embedVar = discord.Embed(description="Sorry, this page does not exist.")
-                await author.send(embed=embedVar)
+        if page_py.exists():
+            embedVar = discord.Embed(
+                title=f"Result:",
+                description=f"%s" % page_py.summary[0:400]
+                + f"[...](https://en.wikipedia.org/wiki/{query})",
+            )
+            await author.send(embed=embedVar)
         else:
-            await ctx.send("None Query Given")
+            embedVar = discord.Embed(description="Sorry, this page does not exist.", hidden=True)
+            await ctx.send(embed=embedVar)
+
 
 
 def setup(client):

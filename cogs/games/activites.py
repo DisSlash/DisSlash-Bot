89955512187c0ffff08c1d5c4564e522c740ce5b
Neil Shah <neildshah1013@gmail.com>
2021-07-07 09:30:36 -0400

    test

--- cogs/games/activites.py
+++ cogs/games/activites.py
@@ -27,22 +27,22 @@ class Activites(commands.Cog):
             )
         ],
     )
-    async def activites(self, ctx, game):
-        print("Yes")
-        if game == "youtube":
-            voice_state = ctx.member.voice
-            if voice_state is None:
-                await ctx.send("You must be in a VC to use this command", hidden=True)
-            else:
+    async def activites(self, ctx, game: str):
+        voice_state = ctx.member.voice
+
+        if voice_state is None:
+            await ctx.send("You must be in a VC to use this command", hidden=True)
+
+        else:
+            if game == "youtube":
+
                 link = await self.togetherControl.create_link(ctx.author.voice.channel.id, 'Youtube')
                 embed = discord.Embed(title="Join The YouTube Together Session!", description=f'Link: {link}')
                 embed.set_image(url='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_(2015-2017).svg/1280px-Logo_of_YouTube_(2015-2017).svg.png')
                 await ctx.send(embed=embed)
-        elif game == "poker":
-            voice_state = ctx.member.voice
-            if voice_state is None:
-                await ctx.send("You must be in a VC to use this command", hidden=True)
-            else:
+
+            elif game == "poker":
+
                 link = await self.togetherControl.create_link(ctx.author.voice.channel.id, 'Youtube')
                 embed = discord.Embed(title="Join The Live Poker Game!", description=f'Link: {link}')
                 embed.set_image(url='https://support.discord.com/hc/article_attachments/1500015218941/Screen_Shot_2021-05-06_at_1.46.50_PM.png')

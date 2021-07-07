e2c4a9d3fc244ba21310e7182eacc6c3fd196e9a
Neil Shah <neildshah1013@gmail.com>
2021-07-07 09:34:51 -0400

    test

--- cogs/games/activites.py
+++ cogs/games/activites.py
@@ -22,14 +22,14 @@ class Activites(commands.Cog):
                 required=True,
                 choices=[
                     create_choice(name="youtube", value="youtube"),
-                    create_choice(name="poker", value="poker"),
-                ],
+                    create_choice(name="poker", value="poker")
+                ]
             )
-        ],
+        ]
     )
     async def activites(self, ctx, game: str):
         voice_state = ctx.member.voice
-
+        print(game)
         if voice_state is None:
             await ctx.send("You must be in a VC to use this command", hidden=True)
 
@@ -47,6 +47,9 @@ class Activites(commands.Cog):
                 embed = discord.Embed(title="Join The Live Poker Game!", description=f'Link: {link}')
                 embed.set_image(url='https://support.discord.com/hc/article_attachments/1500015218941/Screen_Shot_2021-05-06_at_1.46.50_PM.png')
                 await ctx.send(embed=embed)
+            
+            else:
+                print("Wha?")
 
 def setup(client):
     client.add_cog(Activites(client))

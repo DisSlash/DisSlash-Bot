108b2e252fad588723371f2c4859dea8dba6775a
Neil Shah <neildshah1013@gmail.com>
2021-07-31 12:01:26 -0400

    test

--- cogs/text/ticket.py
+++ cogs/text/ticket.py
@@ -1,6 +1,15 @@
+from pymongo.uri_parser import _handle_option_deprecations
+from cogs.games.activities import setup
 from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
 from discord_slash.utils.manage_commands import create_option, create_choice
+import os
+from pymongo import MongoClient
+
+MONGODB = os.environ["MONGODB"]
+cluster = MongoClient(MONGODB)
+db = cluster["disslash"]
+tickets = db["tickets"]
 
 class Ticket(commands.Cog):
   
@@ -13,7 +22,15 @@ class Ticket(commands.Cog):
     )
   
   async def ticket(self, ctx):
-    await ctx.send("Sorry, this command is unavalable as of right now 😔", hidden=True)
+    for i in tickets:
+      setup_bool = i['is_setup']
+      print(setup_bool) 
+    await ctx.send("Done")
+    # if setup_bool:
+    #   await ctx.send("all set up", hidden=True)
+    
+    # else:
+
     
     
 def setup(client):
